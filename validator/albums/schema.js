fb51ba59169df5c6fa980b6d711827e2e7b8bb33
rrtxt <rizfiferdian1@gmail.com>
2024-08-20 15:59:38 +0700

    Initial Commit

--- validator/albums/schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-const AlbumsPayloadSchema = Joi.object({
-  name: Joi.string().required(),
-  year: Joi.number().required()
-})
-
-module.exports = { AlbumsPayloadSchema }
