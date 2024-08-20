523f8dc966ed7cd1bda0fd76d40db7e049de80f9
rrtxt <rizfiferdian1@gmail.com>
2024-08-20 16:10:19 +0700

    initial Commit

---
+++ src/api/albums/handler.js
@@ -0,0 +1,82 @@
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service
+    this._validator = validator
+  }
+
+  async postAlbumHandler(request, h) {
+    this._validator.validatePayload(request.payload)
+
+    const { name, year } = request.payload
+    const albumId = await this._service.addAlbum({ name, year })
+
+    const response = h.response({
+      status: 'success',
+      data: { albumId }
+    })
+
+    response.code(201)
+
+    return response
+  }
+
+  async getAlbumsHandler() {
+    const albums = this._service.getAlbums()
+    const response = h.response({
+      status: 'success',
+      data: {
+        album: albums
+      }
+    })
+
+    response.code(200)
+    return response
+  }
+
+  async getAlbumByIdHandler(request, h) {
+    const { id } = request.params
+    const album = this._service.getAlbumById(id)
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        album
+      }
+    })
+
+    response.code(200)
+    return response
+  }
+
+  async editAlbumByIdHandler(request, h) {
+    this._validator.validatePayload(request.payload)
+    const { name, year } = request.payload
+
+    const albumId = await this._service.editAlbumById({ name, year })
+
+    const response = h.response({
+      status: 'success',
+      message: 'Album berhasil diedit'
+    })
+
+    response.code(200)
+
+    return response
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    const { id } = request.params
+    const albumId = this._service.deleteAlbumById(id)
+
+    const response = h.response({
+      status: 'success',
+      message: `Album dengan id ${albumId} dihapus`
+    })
+
+    response.code(200)
+
+    return response
+  }
+}
+
+module.exports = { AlbumsHandler }
---
+++ src/api/albums/routes.js
@@ -0,0 +1,30 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/albums',
+    handler: (request, h) => handler.postAlbumHandler(request, h)
+  },
+  {
+    method: 'GET',
+    path: '/albums',
+    handler: (request, h) => handler.getAlbumsHandler(request, h)
+  },
+  {
+    method: 'GET',
+    path: '/albums/{id}',
+    handler: (request, h) => handler.getAlbumByIdHandler(request, h)
+  },
+  {
+    method: 'PUT',
+    path: '/albums/{id}',
+    handler: (request, h) => handler.editAlbumByIdHandler(request, h)
+  },
+  {
+    method: 'DELETE',
+    path: '/albums/{id}',
+    handler: (request, h) => handler.deleteAlbumByIdHandler(request, h)
+  }
+
+]
+
+module.exports = routes
