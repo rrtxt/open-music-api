f38e364e0e83bb6de03545eed500dcbdc60b58d4
rrtxt <rizfiferdian1@gmail.com>
2024-08-21 14:48:09 +0700

    Add Song Services

---
+++ src/services/SongsService.js
@@ -0,0 +1,78 @@
+const { nanoid } = require("nanoid");
+const { Pool } = require("pg");
+const InvariantError = require("../exceptions/InvariantError");
+const NotFoundError = require("../exceptions/NotFoundError");
+
+class SongsService {
+  construcor() {
+    this._pool = new Pool()
+  }
+
+  async addSong({ title, year, genre, performer, duration, albumId }) {
+    const id = nanoid(16)
+
+    const query = {
+      text: 'INSERT INTO songs values($1, $2, $3, $4, $5, $6, $7) returning id',
+      values: [id, title, year, performer, genre, duration, albumId]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Song gagal ditambahkan')
+    }
+
+    return result.rows[0].id
+  }
+
+  async getSongs() {
+    const result = await this._pool.query('SELECT * FROM songs')
+
+    return result.rows
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Song tidak ditemukan')
+    }
+
+    return result.rows[0]
+  }
+
+  async editSongById(id, { title, year, genre, performer, duration, albumId }) {
+    const query = {
+      text: 'UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, albumId = $6, WHERE id = $7 returning id',
+      values: [id, title, year, genre, performer, duration, albumId]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui song. Id tidak ditemukan')
+    }
+
+    return result.rows
+  }
+
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM songs WHERE id = $1 returning id',
+      values: [id]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Songs gagal dihapus. Id tidak ditemukan')
+    }
+
+    return result.rows
+  }
+}
