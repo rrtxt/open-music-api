35beddd7e7408130c964c9847e6c34c93019670d
rrtxt <rizfiferdian1@gmail.com>
2024-09-05 11:20:28 +0700

    Lost rack

--- migrations/1725508456566_up.js
+++ migrations/1725508456566_up.js
@@ -8,20 +8,11 @@ exports.shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.up = (pgm) => {
-  pgm.createTable('authentications', {
-    token: {
-      type: 'TEXT',
-      notNull: true
-    }
-  })
-};
+exports.up = (pgm) => {};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.down = (pgm) => {
-  pgm.dropTable('authentications')
-};
+exports.down = (pgm) => {};
--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -38,6 +38,28 @@ class UsersService {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan')
     }
   }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    return id;
+  }
 }
 
 module.exports = UsersService
